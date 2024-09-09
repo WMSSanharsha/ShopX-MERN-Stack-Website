ef1be6dbffcee8dffa623e31358a947ac7a7c94f
Sakila Sanharsha <sakila.sanharsha2k19@gmail.com>
2024-09-09 17:05:48 +0530

    backend done

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.js";
 
 dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
+app.use(express.json());
+
+app.use("/api/products", productRoutes);
+
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server started at http://localhost:5000");
+  console.log("Server started at http://localhost: " + PORT);
 });
